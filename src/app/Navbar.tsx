55dfd5055af7821a4ce52081f65ad3eb6be621e9
K-kaustubh420 <kkaustubh92@gmail.com>
2025-02-27 00:22:56 +0530

    update

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,24 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <div className="navbar bg-base-100">
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-5 w-5 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h16M4 18h16"></path>
-      </svg>
-    </button>
-  </div>
+      <div className="navbar bg-blue-600">
+  
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Proposal submission portal</a>
+  <img src="/Srmlogo.jpg" alt="SRM Logo" className="w-12 h-12 rounded-full" />
+    <a className="btn btn-ghost text-white text-xl">SRM Event Connect</a>
   </div>
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
@@ -34,6 +21,21 @@ const Navbar = () => {
           strokeLinejoin="round"
           strokeWidth="2"
           d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+      </svg>
+    </button>
+  </div>
+  <div className="flex-none">
+    <button className="btn btn-square btn-ghost">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="inline-block h-5 w-5 stroke-current">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"></path>
       </svg>
     </button>
   </div>
