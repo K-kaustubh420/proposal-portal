af9e6a457f828d58c37f7f04a8aaa9a017ec56fc
K-kaustubh420 <kkaustubh92@gmail.com>
2025-03-03 18:26:52 +0530

    update

--- src/app/Proposal/page.tsx
+++ src/app/Proposal/page.tsx
@@ -2,15 +2,17 @@
 import React from 'react'
 import EventProposalForm from './SubmitProposal'
 import Navbar from '@/components/Navbar'
+import ProtectedRoute from '@/components/ProtectedRoute'
 const page = () => {
   return (
     <>
     <nav> 
       <Navbar />
     </nav>
+    <ProtectedRoute>
     <div>
       <EventProposalForm />
-      </div> 
+      </div></ProtectedRoute>
     </>
   )
 }
